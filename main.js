27fc817184e1423b161f47b1fad1f53813050db3
Vera Deeva <verasdee@gmail.com>
2023-10-06 16:05:39 +0700

    feat: begin routing

--- main.js
+++ main.js
@@ -1,23 +1,68 @@
 import 'normalize.css';
 import './style.scss';
-import { Navigation, Thumbs } from 'swiper/modules'
-import Swiper from 'swiper';
-import 'swiper/css';
+import Navigo from 'navigo';
 
-const swiperThumbnails = new Swiper(".product__slider-thumbnails", {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-});
-new Swiper(".product__slider-main", {
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".product__arrow_next",
-        prevEl: ".product__arrow_prev",
-    },
-    modules: [Navigation, Thumbs],
-    thumbs: {
-        swiper: swiperThumbnails,
-    },
-});
+const productSlider = () => {
+    Promise.all([
+        import('swiper/modules'),
+        import('swiper'),
+        import('swiper/css')
+    ]).then(([{ Navigation, Thumbs }, Swiper]) => {
+        const swiperThumbnails = new Swiper.default(".product__slider-thumbnails", {
+            spaceBetween: 10,
+            slidesPerView: 4,
+            freeMode: true,
+            watchSlidesProgress: true,
+        });
+
+        new Swiper.default(".product__slider-main", {
+            spaceBetween: 10,
+            navigation: {
+                nextEl: ".product__arrow_next",
+                prevEl: ".product__arrow_prev",
+            },
+            modules: [Navigation, Thumbs],
+            thumbs: {
+                swiper: swiperThumbnails,
+            },
+        });
+    });
+};
+
+
+
+const init = () => {
+    productSlider();
+
+    const router = new Navigo("/", { linksSelector: 'a[href^="/"]' });
+
+    router
+        .on("/", () => {
+            console.log('на главной')
+        })
+        .on("/category", (obj) => {
+            console.log('category', obj);
+        })
+        .on("/favorite", () => {
+            console.log('favorite');
+        })
+        .on("/search", () => {
+            console.log('search');
+        })
+        .on("/product/:id", (obj) => {
+            console.log('obj:  ', obj)
+        })
+        .on("/cart", () => {
+            console.log('cart');
+        })
+        .on("/order", () => {
+            console.log('order');
+        })
+        .notFound(() => {
+            console.log(404);
+        })
+
+    router.resolve();
+};
+
+init();
